b1deb9d383fd5a08853338595107cad4746c1cd5
Sol <trust2005@ukr.net>
2015-04-28 17:13:24 -0400

    Added options
    1. Added options
    'mode' = [clear, dupes, all] - eliminate showed data
    'original' - say to take XML data from storage (column 'xml')
    filename - set file name for output
    2. Added logic to populate XML from stored xml data

--- addons/export.py
+++ addons/export.py
@@ -7,7 +7,8 @@ from lxml.builder import E as buildE
 import logging
 # import lxml.usedoctest
 # import xml.etree.cElementTree as ET
-
+import argparse # optparse
+import time
  
 PATH = os.path.abspath('.') +'/'
 MYSQL_CONFIG = PATH + 'db.cnf'
@@ -17,49 +18,48 @@ INTEGER_COLUMNS = ['id']
 FILE_EXTENSION  = ['csv', 'xml', 'json']
 DUPES_FILE = PATH + 'dupes.' + FILE_EXTENSION[1]
 PRETTY_PRINT = False
+ORIGINAL_XML_COLUMN = 'xml'
 TABLE_COLUMNS =['id', 'reference_type', 'author', 'year', 'title', 'pages', 'secondary_title', 'volume', 'number', 'keywords', 'date', 
-'abstract', 'url', 'notes', 'isbn', 'alternate_title', 'accession_number', 'author_address', 'language', 'database_provider',
-'name_of_database', ]
+'abstract', 'url', 'notes', 'isbn', 'alternate_title', 'accession_number', 'author_address', 'language', 'database_provider', 'name_of_database', ]
 """
-database
-source-app
-    key
-ref-type
-author
-auth-address
-title
-secondary-title
-alt-title
-    periodical
-        full-title
-        abbr-1
-    alt-periodical
-        full-title
-        abbr-1
-pages
-volume
-number
-keyword
-year
-date
-isbn
-accession-num
-abstract
-notes
-url
-remote-database-name
-remote-database-provider
-language
+    database
+    source-app
+        key
+    ref-type
+    author
+    auth-address
+    title
+    secondary-title
+    alt-title
+        periodical
+            full-title
+            abbr-1
+        alt-periodical
+            full-title
+            abbr-1
+    pages
+    volume
+    number
+    keyword
+    year
+    date
+    isbn
+    accession-num
+    abstract
+    notes
+    url
+    remote-database-name
+    remote-database-provider
+    language
 """
 
 CLEAR_DATA_FILE = PATH + 'clear_data.' + FILE_EXTENSION[1]
-TABLE_NAME = 'refs' #'refs_training' # 'refs' #"test_upload"
+TABLE_NAME = 'test_upload' #'refs_training' # 'refs' #"test_upload"
 tabPK = "id"
 LINE_SEPARATOR = '\r' # &#13 '\r\n'
 
 GROUPED_ELEMENTS  = {'key'            : 'foreign-keys',
                     'authors'         : 'contributors',
-                   # 'contributors'    : 'test',
                     'author'          : 'authors',
                     'keyword'         : 'keywords',
                     'title'           : 'titles',
@@ -73,7 +73,7 @@ GROUPED_ELEMENTS  = {'key'            : 'foreign-keys',
                     'related-urls'    : 'urls',
                     'url'             : 'related-urls'}
                     
-LIST_ELEMENTS   = ['author', 'keyword', 'url'] #, 'title', 'secondary-title', 'alt-title']
+LIST_ELEMENTS   = ['author', 'keyword', 'url']
 STYLED_ELEMENTS = ['author', 'auth-address', 'title', 'secondary-title', 'alt-title', 'full-title', 'abbr-1', 'pages', 'volume', 'number', 'keyword', 'year', 'date', 'isbn', 'accession-num', 'abstract', 'notes', 'url', 'remote-database-name', 'remote-database-provider', 'language']
 
 # commented for same names in pair Tag-Column
@@ -114,35 +114,63 @@ class export(object):
             # self.resetAnalyzer()
             # sys.exit(0)
 
-        self.outFile = outFile
-        self.tableName = tabName
-        self.recCount = 0
-        self.fields = {MAP_COLUMN_TO_XML[k] : k for k in MAP_COLUMN_TO_XML}     #Invert into Column-Tag mapping
+        self.outFile    = outFile
+        self.tableName  = tabName
+        self.tablePK    = tabPK
+        self.limit      = 999999
+        self.constraint = ''
+        self.columns    = ', '.join(TABLE_COLUMNS)
+        self.elements   = {MAP_COLUMN_TO_XML[k] : k for k in MAP_COLUMN_TO_XML}     #Invert into Column-Tag mapping
+        self.recCount   = 0
         
         self.con = self.connectDB(MYSQL_CONFIG)
         self.cursor = self.con.cursor(dictionary = True, buffered = False)
         self.cursor.execute("SET net_write_timeout = 3600")
 
     def startExport(self):
-        data = {}
-        self.loadData()
+        # data = {}
+        res = self.loadData(1000)                                         # Load Data from DB with limitation of output
         data = self.cursor
-        print(', '.join(TABLE_COLUMNS))
-        
-        if data != None:
+
+        if data != None and res != 0:
             logging.info('Data from Table %s were loaded.' % self.tableName)
-            
-            doc = self.fetchXML() #data)
+
+            doc = self.generateXML()
 
             if self.writeXmlFile(doc, self.outFile):
                 logging.info('Data were successfuly exported into file %s' % self.outFile)
             else:
-                logging.warning('Cannont write Data into file %s' % self.outFile)
+                logging.warning('Cannot write Data into file %s' % self.outFile)
 
             self.closeAllConnections
         else:
-            logging.warning('Cannont read data from Database Table %s' % self.tableName)
+            logging.warning('Cannot read data from Database Table %s' % self.tableName)
             self.resetExport()
+
+    def startExportFromOriginal(self):
+        # data = {}
+        self.loadData(self.limit)                                                      # Load Data from DB with limitation of output
+        data = self.cursor
+        
+        if data != None and self.columns in data.description:
+            logging.info('Original XML data from Table %s were loaded.' % self.tableName)
+
+            with open(self.outFile, 'w') as f:
+                print('<?xml version="1.0" encoding="UTF-8" ?><xml><records>', file=f) # Open XML tags
+                for xml_string in data.fetchall():
+                    print(xml_string[self.columns], file=f)                            # Read original XML from DB
+                    self.recCount += 1
+                print('</records></xml>', file=f)                                      # Close XML tags
+            
+                logging.info('Data were successfuly exported into file %s' % self.outFile)
+                return True
+
+            logging.warning('Cannot write Data into file %s' % self.outFile)
+            self.closeAllConnections
+        else:
+            logging.warning('Cannot read original XML data from Database Table %s' % self.tableName)
+            self.resetExport()
+        return False
             
     def writeXmlFile(self, data, outFile):
         with open(outFile, 'w') as f:
@@ -150,10 +178,8 @@ class export(object):
             return True
  
         return False
-            
-        # schema = etree.XMLSchema(file=SCHEMA_FILE)
-        # export = objectify.makeparser(schema=schema)
-    def addStyle(self, tag, parent=None, content=None): # or addSubElement
+
+    def addStyle(self, tag, parent=None, content=None):             # Add Style element
         style = buildE(tag)
 
         if tag == 'style':
@@ -165,9 +191,8 @@ class export(object):
             style.text = str(content)
         if parent is not None:
             parent.append(style)
-        # return style
 
-    def addGroup(self, leafName, parent=None):
+    def addGroup(self, leafName, parent=None):                      # Add Group's structure
         root = None
         childBranch = None
         parentBranch = None
@@ -210,11 +235,10 @@ class export(object):
             parent.append(root)                             # Add Element(child|parent) to Parent(parent|grandParent)
         return childBranch
     
-    # Add Leaf-data to branch
-    def addElement(self, tag, parent=None, content=None):
-        element = buildE(tag)                               # Create Element
+    def addElement(self, tag, parent=None, content=None):           # Add content data as leaf to branch
+        element = buildE(tag)                                       # Create Element
         if content is not None:
-            if tag in STYLED_ELEMENTS:                      # Check if needed to add Style element
+            if tag in STYLED_ELEMENTS:                              # Check if needed to add Style element
                 self.addStyle('style', element, str(content))
             else:
                 element.text = str(content)
@@ -223,23 +247,24 @@ class export(object):
             parent.append(element)
         return element
 
-    def fetchXML(self):
+    def generateXML(self):
         doc = self.addElement('xml')
         records = self.addElement('records', parent=doc)
-
+        
         for record in self.cursor.fetchall():
             r = self.addElement('record', parent=records)
+
             # If posible replace this HARDCODE block
-            d = self.addElement('database', content=self.outFile, parent=r)
+            d = self.addElement('database', content=self.outFile, parent=r)         # Add Tag 'database'
             d.attrib['name'] = self.outFile
             d.attrib['path'] = PATH + self.outFile
             
-            s = self.addElement('source-app', content='EndNote', parent=r)
+            s = self.addElement('source-app', content='EndNote', parent=r)          # Add Tag 'source-app'
             s.attrib['name'] = 'EndNote'
             s.attrib['version'] = '17.2'
             
             for k, content in record.items():
-                elementName = self.fields.get(k, k)
+                elementName = self.elements.get(k, k)
 
                 # Create tree branches
                 if elementName in GROUPED_ELEMENTS:                                 # If Element should be in Group of elements
@@ -253,17 +278,30 @@ class export(object):
                         contents = content.split(LINE_SEPARATOR)                    # Create List - Fix separator same as in Refs Table!
                         for value in contents:
                             self.addElement(elementName, branch, str(value))        # Add elements from list
-                            # print(value)
                     else:
                         self.addElement(elementName, branch, str(content))          # Add regular elements
 
             self.recCount += 1
         return doc
 
-    def loadData(self, limit = 1000):
-        q = "SELECT %s FROM %s t LIMIT %s" % (', '.join(TABLE_COLUMNS), self.tableName, limit)
-        self.cursor.execute(q)
-        return self.cursor.rowcount
+    def loadData(self, limit):
+        sql = "SELECT %s FROM %s %s LIMIT %s" % (self.columns, self.tableName, self.constraint, limit)
+        
+        if self.constraint != '':
+            self.cursor.execute("SHOW TABLES LIKE  '%s_entity_map'" % self.tableName)
+            # self.cursor.execute("SELECT 1 FROM information_schema.tables WHERE table_name ='%s'" % self.tableName)
+
+            if self.cursor.fetchone():
+                print(sql)
+                self.cursor.execute(sql)
+                # return len(self.cursor.fetchall()) 
+                return self.cursor.rowcount # & 0xFFFFFFFFFFFFFFFF # Added to solve problem with Wrong rowcount
+            else:
+                logging.warning("WARNING! Couldn't find table %s_entity_map. Please, run Analyser first." % self.tableName)
+                return 0
+        else:
+            self.cursor.execute(sql)
+            return self.cursor.rowcount #& 0xFFFFFFFFFFFFFFFF # Added to solve problem with Wrong rowcount
 
     def connectDB(self, dbOptionsFile):
         """ You need to fill option file `db.cnf` with your mysql database information """
@@ -281,19 +319,65 @@ class export(object):
         self.closeAllConnections
 
 def start(xmlFile = XML_FILE):
-    initLogging(logging.INFO)
     global export
-    export = export(xmlFile)       # initiate analyser
-    export.startExport()
-    print('%s records were exported' % export.recCount)
-    # schema = etree.XMLSchema(file=SCHEMA_FILE)
-    # export = objectify.makeparser(schema=schema)
+    # initLogging(logging.INFO)
+    log_level = logging.WARNING
+    start_time = time.time()                    # set start time
+
+    opt = options()
+    if opt :
+        if opt.verbose :
+            if opt.verbose == 1:
+                log_level = logging.INFO
+            elif opt.verbose >= 2:
+                log_level = logging.DEBUG
+        initLogging(log_level)                  # start logging
+        
+        if opt.filename is not None:
+            xmlFile = opt.filename
+        export = export(xmlFile)                # initiate export
+        
+        if opt.mode == 'clear':
+            export.constraint = ' WHERE %(id)s NOT IN (SELECT %(id)s FROM %(tabName)s_entity_map)' % {'id': export.tablePK, 'tabName': export.tableName}
+        elif opt.mode == 'dupes':
+            export.constraint = ' WHERE %(id)s IN (SELECT %(id)s FROM %(tabName)s_entity_map)' % {'id': export.tablePK, 'tabName': export.tableName}
+        else:
+            export.constraint = ''
+        
+        if opt.fromOriginal:
+            export.columns = ORIGINAL_XML_COLUMN
+            export.startExportFromOriginal()            # Export from stored Original XML data
+        else:
+            export.columns = ', '.join(TABLE_COLUMNS)   # Generate columns from a list
+            export.startExport()                        # Export by generating new XML file
+    else:
+        initLogging(log_level)                          # Start logging
+        export.startExport()                            # Export by generating new XML file
     
-    # if validateXml(CLEAR_DATA_FILE, export):
-        # print("URA!")
-            
-# clear xml object
-def clear_element(element):
+    print('%s records were exported' % export.recCount)
+
+def options():                                          # Add parameters
+    parser = argparse.ArgumentParser(description='Export Data from Database into XML file. '+
+   ' It can be used in Training mode for machinery learning(creating clusters with weight for each similar records pairs). '+
+   ' For setup it uses Data Model and loads it from file in .JSON format.'+ 
+   ' As output it generates mapping table and stores there all duplicate records.')
+    parser.add_argument('filename',  default=XML_FILE)
+    parser.add_argument('-m', '--mode', dest='mode', default='clear',  choices=['all', 'clear', 'dupes'], action='store',
+                    help='''Set mode for output data into XML file
+                    "all"   - output all data (same as input file)
+                    "clear" - output clear data without dupes
+                    "dupes" - output only duplicated records'''
+                    )
+    parser.add_argument('-o', '--original', dest='fromOriginal', action='store_true',
+                    help='Use it to output XML file from stored original XML data'
+                    )
+    parser.add_argument('-v', '--verbose', dest='verbose', action='count',
+                    help='Increase verbosity (specify twice for DEBUG)'
+                    )
+
+    return parser.parse_args()
+
+def clear_element(element):                             # Clear xml object
     element.clear()
     while element.getprevious() is not None:
         del element.getparent()[0]
@@ -307,7 +391,7 @@ def fast_iter(context, cursor):
             # for authors in element.find("authors"):# element.findall("authors"):
             if len(list(element))>0 and element.tag == 'authors':
                 for author in list(element):
-                    print(author.text)
+                    pass
                 print(list(element).getelementpath(element))
             paper = {
                 'element' : element.tag,
@@ -326,12 +410,7 @@ def fast_iter(context, cursor):
                 pass
 
             paperCounter += 1
-
             clear_element(element)
-
-# tree = etree.ElementTree(root)
-# for e in root.iter():
-    # print tree.getpath(e)
 
 def validateXml(xmlFile, schemaFile = SCHEMA_FILE):
     schema = etree.XMLSchema(etree.parse(schemaFile))
@@ -354,4 +433,4 @@ def initLogging(log_level = logging.WARNING):
     logging.getLogger().setLevel(log_level)
 
 if __name__ == '__main__':
-    start('data_output.xml')
+    start()
